8875d0d56627f111d7fe9d5da15373a5719cdd63
Bisvarup Mukherjee <bisvarup.mukherjee@tracxn.com>
2019-04-17 23:26:46 +0530

    basic project without validation

--- backend/src/features/branch/controller/index.js
+++ backend/src/features/branch/controller/index.js
@@ -1,7 +1,26 @@
-class Branch {
-    getIndex(req, res, next) {
-        // if (!errors.isEmpty()) this.errorCodes.create422(res);
+const Controller = require("../../../lib/generic/controller")
+class Branch extends Controller {
 
+    getBranchDetails(ifsc,res) {
+        this.findWhere({select: "*",table: "postgres", where: `ifsc=${ifsc}`})
+        .then(result=>console.log({result}))
+        .catch(err=>console.log({err}))
+    }
+
+    listBranches(bank,city, res){
+        this.findWhere({select: "*",table: "postgres", where: `bank=${bank} and city=${city}`})
+        .then(result=>console.log({result}))
+        .catch(err=>console.log({err}))
+    }
+
+    getIndex(req, res, next) {
+        const {ifsc, bank, city} = req.query
+
+        if(ifsc) {
+            this.getBranchDetails(ifsc,res)
+        } else if(bank && city) {
+            this.listBranches(bank,city,res)
+        }
         res.json({status: "Branch System working"})
     }
 }
--- backend/src/lib/generic/controller.js
+++ backend/src/lib/generic/controller.js
@@ -1,63 +1,11 @@
+const client = require("../../model/postgres/db")
 class Controller {
-    constructor(facade) {
-        this.facade = facade;
-    }
 
-    create(req, res, next) {
-        this.facade.create(req.body).then(doc => res.status(201).json(doc), err => next(err));
-    }
+    findWhere({select, table, where}) {
+        const query = "SELECT $1::text from $2::text WHERE $3::text"
+        const needle = [select, table, where]
 
-    find(req, res, next) {
-        return this.facade
-            .find(req.query)
-            .then(collection => res.status(200).json(collection))
-            .catch(err => next(err));
-    }
-
-    findOne(req, res, next) {
-        return this.facade
-            .findOne(req.query)
-            .then(doc => res.status(200).json(doc))
-            .catch(err => next(err));
-    }
-
-    findById(req, res, next) {
-        return this.facade
-            .findById(req.params.id)
-            .then(doc => {
-                if (!doc) {
-                    return res.sendStatus(404);
-                }
-                return res.status(200).json(doc);
-            })
-            .catch(err => next(err));
-    }
-
-    update(req, res, next) {
-        this.facade
-            .update({ _id: req.params.id }, req.body)
-            .then(results => {
-                if (results.n < 1) {
-                    return res.sendStatus(404);
-                }
-                if (results.nModified < 1) {
-                    return res.sendStatus(304);
-                }
-                return res.sendStatus(204);
-            })
-            .catch(err => next(err));
-    }
-
-    remove(req, res, next) {
-        this.facade
-            .remove({ _id: req.params.id })
-            .then(doc => {
-                if (!doc) {
-                    return res.sendStatus(404);
-                }
-                return res.sendStatus(204);
-            })
-            .catch(err => next(err));
+        return client.query(query,needle)
     }
 }
 
