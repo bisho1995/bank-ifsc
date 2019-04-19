9a30831011cb256b1450161f590a578adc727ee0
Bisvarup Mukherjee <bisvarup.mukherjee@tracxn.com>
2019-04-19 23:54:56 +0530

    handle all banks of city and bank name + better message

--- backend/src/features/branch/controller/index.js
+++ backend/src/features/branch/controller/index.js
@@ -1,7 +1,6 @@
 const client = require("../../../model/postgres/db")
 const errorCodes = require("../../../lib/errorCodes/errorCodes")
-const Controller = require("../../../lib/generic/controller")
-class Branch extends Controller {
+class Branch{
 
     getBranchDetails(ifsc,res) {
         const query = `SELECT br.ifsc,ba.name as bank_name,br.district,br.branch,br.address,br.city,br.state  from branches br inner join banks ba on ba.id=br.bank_id where ifsc=$1`
@@ -11,7 +10,7 @@ class Branch extends Controller {
         .then(({rows})=>{
             if(rows.length)
                 res.status(200).send({status: "success",payload: rows[0]})
-            else res.status(400).send({status: "failure",message: "The ifsc code does not exist`"})
+            else res.status(400).send({status: "failure",message: "Could not find any results. The ifsc code does not exist`"})
         })
         .catch(err=> {
             console.log(err)
@@ -20,8 +19,15 @@ class Branch extends Controller {
     }
 
     listBranches(bank,city, res){
-        this.findWhere({select: "*",table: "postgres", where: `bank=${bank} and city=${city}`})
-        .then(result=>console.log({result}))
+        const query = `SELECT br.ifsc,ba.name as bank_name,br.district,br.branch,br.address,br.city,br.state  from branches br inner join banks ba on ba.id=br.bank_id where ba.name=$1 and br.city=$2`
+        const needle = [bank,city]
+
+        client.query(query,needle)
+        .then(({rows})=>{
+            if(rows.length)
+                res.status(200).send({status: "success",payload: rows})
+            else res.status(400).send({status: "failure",message: "Could not find any results. The bank name, city or both may be incorrect`"})
+        })
         .catch(err=> {
             console.log(err)
             errorCodes.create500(res)
