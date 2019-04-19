75826ffb94fd190e4aac6949b45fdbf5bda301bc
Bisvarup Mukherjee <bisvarup.mukherjee@tracxn.com>
2019-04-19 23:36:43 +0530

    updated error messages

--- backend/src/lib/errorCodes/errorCodes.js
+++ backend/src/lib/errorCodes/errorCodes.js
@@ -1,15 +1,15 @@
 class ErrorCodes {
   create500(res) {
-    res.status(500).send({ status: 'Internal server error' });
+    res.status(500).send({ status: "failure", message: 'Internal server error' });
   }
 
   create400(res) {
-    res.status(400).send({ status: 'Operation not supported' });
+    res.status(400).send({ status: "failure", message: 'Operation not supported' });
   }
 
   create422(res) {
-    res.status(422).send({ status: 'Unprocessable entity.' });
+    res.status(422).send({ status: "failure", message: 'Unprocessable entity.' });
   }
 }
 
-module.exports = ErrorCodes;
+module.exports = new ErrorCodes();
