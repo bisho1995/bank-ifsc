d1347c9e0c5370e54274e067cc3b8e64dbb5bc62
Bisvarup Mukherjee <bisvarup.mukherjee@tracxn.com>
2019-04-19 22:35:28 +0530

    got postgres working locally

--- backend/src/model/postgres/db.js
+++ backend/src/model/postgres/db.js
@@ -2,13 +2,15 @@ const pg = require('pg')
 const {Client} = pg
 
 const client = new Client({
-    password: "postgres",
-    user: "postgres",
-    host: "postgres",
+    password: "password",
+    user: "bisho",
+    host: "0.0.0.0",
+    database:"ifsc",
+    port: 5433
 });
 
 client.connect()
     .then(()=>console.log("Connected to postgres", ))
-    .catch(err=>console.log("could not connect to postgres ",err))
+    .catch(err=>console.log("COULD not connect to postgres ",err))
 
 module.exports = client
